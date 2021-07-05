08ac2b79506647da2b4b8b82f120f98c81fe4a13
leo <liyang.ok@outlook.com>
2021-07-05 11:26:12 +0800

    init

--- app/src/main/java/com/top/imebra/MainActivity.kt
+++ app/src/main/java/com/top/imebra/MainActivity.kt
@@ -3,9 +3,11 @@ package com.top.imebra
 import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.imebra.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,6 +33,8 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(Intent.createChooser(intent, "Select a DICOM file"), 123)
     }
 
+    var selectedfile: Uri? = null
+    var count=0;
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -40,66 +44,10 @@ class MainActivity : AppCompatActivity() {
                 CodecFactory.setMaximumImageSize(8000, 8000)
 
                 // Get the selected URI, then open an input stream
-                val selectedfile = data!!.data ?: return
-                val stream = contentResolver.openInputStream(selectedfile)
+                selectedfile = (data!!.data ?: return)
 
-                // The usage of the Pipe allows to use also files on Google Drive or other providers
-                val imebraPipe = PipeStream(32000)
+                goToImg(count)
 
-                // Launch a separate thread that read from the InputStream and pushes the data
-                // to the Pipe.
-                val pushThread = Thread(PushToImebraPipe(imebraPipe, stream))
-                pushThread.start()
-
-                // The CodecFactory will read from the Pipe which is feed by the thread launched
-                // before. We could just pass a file name to it but this would limit what we
-                // can read to only local files
-                val loadDataSet = CodecFactory.load(StreamReader(imebraPipe.streamInput))
-
-
-                // Get the first frame from the dataset (after the proper modality transforms
-                // have been applied).
-                val dicomImage = loadDataSet.getImageApplyModalityTransform(0)
-
-                // Use a DrawBitmap to build a stream of bytes that can be handled by the
-                // Android Bitmap class.
-                val chain = TransformsChain()
-                if (ColorTransformsFactory.isMonochrome(dicomImage.colorSpace)) {
-                    val voilut = VOILUT(
-                        VOILUT.getOptimalVOI(
-                            dicomImage,
-                            0,
-                            0,
-                            dicomImage.width,
-                            dicomImage.height
-                        )
-                    )
-                    chain.addTransform(voilut)
-                }
-                val drawBitmap = DrawBitmap(chain)
-                val memory = drawBitmap.getBitmap(dicomImage, drawBitmapType_t.drawBitmapRGBA, 4)
-
-                // Build the Android Bitmap from the raw bytes returned by DrawBitmap.
-                val renderBitmap = Bitmap.createBitmap(
-                    dicomImage.width.toInt(), dicomImage.height.toInt(), Bitmap.Config.ARGB_8888
-                )
-                val memoryByte = ByteArray(memory.size().toInt())
-                memory.data(memoryByte)
-                val byteBuffer = ByteBuffer.wrap(memoryByte)
-                renderBitmap.copyPixelsFromBuffer(byteBuffer)
-
-                // Update the image
-                iv.setImageBitmap(renderBitmap)
-                iv.scaleType = ImageView.ScaleType.FIT_CENTER
-
-                // Update the text with the patient name
-                tv.setText(
-                    loadDataSet.getPatientName(
-                        TagId(0x10, 0x10),
-                        0,
-                        PatientName("Undefined", "", "")
-                    ).alphabeticRepresentation
-                )
             } catch (e: IOException) {
                 val dlgAlert = AlertDialog.Builder(this)
                 dlgAlert.setMessage(e.message)
@@ -116,4 +64,74 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    fun pre(view: View) {
+        goToImg(--count)
+
+    }
+    fun next(view: View) {
+        goToImg(++count)
+    }
+
+
+
+    fun goToImg(index:Int){
+        val stream = contentResolver.openInputStream(selectedfile!!)
+
+        // The usage of the Pipe allows to use also files on Google Drive or other providers
+        val imebraPipe = PipeStream(32000)
+
+        // Launch a separate thread that read from the InputStream and pushes the data
+        // to the Pipe.
+        val pushThread = Thread(PushToImebraPipe(imebraPipe, stream))
+        pushThread.start()
+
+
+        val loadDataSet = CodecFactory.load(StreamReader(imebraPipe.streamInput))
+        Toast.makeText(this,"size: "+loadDataSet.tags.size,Toast.LENGTH_LONG).show()
+
+
+        // Get the first frame from the dataset (after the proper modality transforms
+        // have been applied).
+        val dicomImage = loadDataSet.getImageApplyModalityTransform(index.toLong())
+
+        // Use a DrawBitmap to build a stream of bytes that can be handled by the
+        // Android Bitmap class.
+        val chain = TransformsChain()
+        if (ColorTransformsFactory.isMonochrome(dicomImage.colorSpace)) {
+            val voilut = VOILUT(
+                VOILUT.getOptimalVOI(
+                    dicomImage,
+                    0,
+                    0,
+                    dicomImage.width,
+                    dicomImage.height
+                )
+            )
+            chain.addTransform(voilut)
+        }
+        val drawBitmap = DrawBitmap(chain)
+        val memory = drawBitmap.getBitmap(dicomImage, drawBitmapType_t.drawBitmapRGBA, 4)
+
+        // Build the Android Bitmap from the raw bytes returned by DrawBitmap.
+        val renderBitmap = Bitmap.createBitmap(
+            dicomImage.width.toInt(), dicomImage.height.toInt(), Bitmap.Config.ARGB_8888
+        )
+        val memoryByte = ByteArray(memory.size().toInt())
+        memory.data(memoryByte)
+        val byteBuffer = ByteBuffer.wrap(memoryByte)
+        renderBitmap.copyPixelsFromBuffer(byteBuffer)
+
+        // Update the image
+        iv.setImageBitmap(renderBitmap)
+        iv.scaleType = ImageView.ScaleType.FIT_CENTER
+
+        // Update the text with the patient name
+        tv.text = loadDataSet.getPatientName(
+            TagId(0x10, 0x10),
+            index.toLong(),
+            PatientName("Undefined", "", "")
+        ).alphabeticRepresentation
+    }
+
 }
